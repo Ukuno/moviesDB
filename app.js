79f36b4f90a822bbe600600991b88dbc2fd99a02
ukuno <yukunos123@gmail.com>
2018-03-31 18:27:54 -0700

    add input form and list all output data

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express'),
     movieDB = require('moviedb')('15d2ea6d0dc1d476efbca3eba2b9bbfb'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
     app = express();
 
 app.set("view engine", "ejs");
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/", (req, res) => {
+    res.render("index");
 
 });
 
-app.get("/search_result", (req, res) => {
-    movieDB.searchMovie({ query: 'the first lord of the rings' }, (err, result) => {
+app.post("/search", (req, res) => {
+    let query = req.body.search;
+
+    movieDB.searchMovie({ query: `${query}` }, (err, result) => {
 
         // console.log(MOVIEDBAPIKEY);
         if (err || result.total_results == 0) {
